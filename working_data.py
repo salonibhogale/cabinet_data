defb226d4f0c7bbbc72f0093da9d6eced7d15f72
keyakgupta <66508798+keyakgupta@users.noreply.github.com>
2020-06-08 14:43:45 +0530

    Changes to working_data

--- working_data.py
+++ working_data.py
@@ -1,4 +1,4 @@
-from typing import Any, Union
+
 
 import pandas as pd
 
@@ -10,23 +10,21 @@ import pandas as pd
 # for example for house:
 # unique_values_for_column_house = set(cabinet_csv_file.house)
 # print(unique_values_for_column_house)
-from pandas import Series, DataFrame
-from pandas.io.parsers import TextFileReader
 
-cabinet_csv_file = pd.read_csv("cabinet_data_consistency_1205_26M.csv")
 
+cabinet_csv_file = pd.read_csv("cabinet_data_final_0406_v2.csv")
+unique_values_for_name = set(cabinet_csv_file.name)
+print(unique_values_for_name)
 unique_values_for_state = set(cabinet_csv_file.state)
 print(unique_values_for_state)
 #  Punjab and Punajb,
-u=set(cabinet_csv_file.num_of_years)
-print(u)
+
 unique_values_for_constituency = set(cabinet_csv_file.constituency)
 print(unique_values_for_constituency)
 unique_values_for_house = set(cabinet_csv_file.house)
 print(unique_values_for_house)
 # nan for house
-unique_values_for_year = set(cabinet_csv_file.year)  # Why is the year appearing as only nan?
-print(unique_values_for_year)
+
 unique_values_for_party = set(cabinet_csv_file.party)
 print(unique_values_for_party)
 # SAMATA PARTY and Samata Party(some parties are in lowercase while others are in uppercase
@@ -34,8 +32,6 @@ unique_values_for_gender = set(cabinet_csv_file.gender)
 print(unique_values_for_gender)
 unique_values_for_ls_number = set(cabinet_csv_file.ls_number)
 print(unique_values_for_ls_number)
-unique_values_for_department_update = set(cabinet_csv_file.department_update)
-print(unique_values_for_department_update)
 
 
 # nan for ls number
@@ -62,7 +58,7 @@ cabinet_csv_file['state']= cabinet_csv_file['state'].str.upper()
 # replace all spaces in party column - ' ' to '' --> Google
 
 for col in cabinet_csv_file.columns:
-    if col in ['name','gender','party','ministry','department','department_update','rank','rank_comment','party.1','house','constituency','state']:
+    if col in ['name','gender','party','department','rank','rank_comment','house','constituency','state']:
         cabinet_csv_file.rename(columns={col: col.upper()}, inplace=True)
 
 #cabinet_csv_file.groupby('state').count()
@@ -75,11 +71,73 @@ cabinet_csv_file.loc[cabinet_csv_file['PARTY']=='BANGLA CONG','PARTY']= 'BC'
 cabinet_csv_file['PARTY']= cabinet_csv_file['PARTY'].str.replace(' ','')
 updated_unique_values_for_party = set(cabinet_csv_file.PARTY)
 print(updated_unique_values_for_party)
+# 4-June
+# See what are the unique values for each column
+
+# cleaning column ls_number
+print(unique_values_for_ls_number)
+#cleaning column for NAME
+cabinet_csv_file.loc[pd.isnull(cabinet_csv_file['NAME']) == True, 'NAME'] = 'NOT_AVAILABLE'
+cabinet_csv_file.loc[cabinet_csv_file['NAME']=='N/a','NAME']='NOT_AVAILABLE'
+#cleaning column for GENDER
+print(unique_values_for_gender)
+#cleaning column for PARTY
+print(updated_unique_values_for_party)
+bool_series_party = pd.isnull(cabinet_csv_file['PARTY'])
+print(cabinet_csv_file.loc[pd.isnull(cabinet_csv_file['PARTY'])])
+cabinet_csv_file.loc[pd.isnull(cabinet_csv_file['PARTY']) == True, 'PARTY'] = 'NOT_APPLICABLE'
+cabinet_csv_file.loc[cabinet_csv_file['PARTY']=='N/a','PARTY']='NOT_APPLICABLE'
+
+
+cabinet_csv_file.groupby('PARTY').count()
+#cleaning column for appointment_begin
+cabinet_csv_file.loc[pd.isnull(cabinet_csv_file['appointment_begin']) == True, 'appointment_begin'] = 'NOT_AVAILABLE'
+cabinet_csv_file.loc[cabinet_csv_file['appointment_begin']=='N/a','appointment_begin']='NOT_AVAILABLE'
+
+unique_values_for_appointment_begin= set(cabinet_csv_file.appointment_begin)
+print(unique_values_for_appointment_begin)
+bool_series_appointment_begin = pd.isnull(cabinet_csv_file['appointment_begin'])
+set_of_boolean_series_appointment_begin =set(bool_series_appointment_begin)
+sum_appointment_begin=sum(set_of_boolean_series_appointment_begin)
+print(sum_appointment_begin)#no nan in appointment begin
+
+#cleaning column for appointment_end
+cabinet_csv_file.loc[pd.isnull(cabinet_csv_file['appointment_end']) == True, 'appointment_end'] = 'NOT_AVAILABLE'
+cabinet_csv_file.loc[cabinet_csv_file['appointment_end']=='N/a','appointment_end']='NOT_AVAILABLE'
+bool_series_appointment_end = pd.isnull(cabinet_csv_file['appointment_end'])
+set_of_boolean_series_appointment_end = set(bool_series_appointment_end)
+sum_appointment_end= sum(set_of_boolean_series_appointment_end)
+print(sum_appointment_begin)#no nan in appointment_end
+#cleaning column for rank
+unique_values_for_RANK=set(cabinet_csv_file.RANK)
+print(unique_values_for_RANK)
+#cleaning column for rank_comment
+unique_values_for_rank_comment= set(cabinet_csv_file.RANK_COMMENT)
+print(unique_values_for_rank_comment)
+cabinet_csv_file.loc[pd.isnull(cabinet_csv_file['RANK_COMMENT']) == True, 'RANK_COMMENT'] = 'NOT_APPLICABLE'
+#cleaning column for constituecy
+cabinet_csv_file.loc[pd.isnull(cabinet_csv_file['CONSTITUENCY']) == True, 'CONSTITUENCY'] = 'NOT_APPLICABLE'
+#cleaning column for state
+cabinet_csv_file.loc[pd.isnull(cabinet_csv_file['STATE']) == True, 'STATE'] = 'NOT_APPLICABLE'
+#finding out the difference between appointment_begin and appointment_end
+from datetime import datetime
+
+cabinet_csv_file['appointment_begin_in_datetime']= pd.to_datetime(cabinet_csv_file['appointment_begin'], format="%A, %d %B %Y")
+#changing some particular data in the column appointment_end
+cabinet_csv_file.loc[cabinet_csv_file['appointment_end']=='Tuesday, 4 November, 1969','appointment_end']='Tuesday, 4 November 1969'
+cabinet_csv_file['appointment_end_in_datetime']= pd.to_datetime(cabinet_csv_file['appointment_end'], format='%A, %d %B %Y')
+
+import numpy as np
+cabinet_csv_file['appointment_difference_in_years'] = cabinet_csv_file['appointment_end_in_datetime'] - cabinet_csv_file['appointment_begin_in_datetime']
+cabinet_csv_file['appointment_difference_in_years'] = cabinet_csv_file['appointment_difference_in_years'] / np.timedelta64(1, 'Y')
+print(cabinet_csv_file['appointment_difference_in_years'])
 
 
 
 
-#unique_values_for_house = set(cabinet_csv_file.house)
+
+#print(bool_series_appointment_end)
+
 
 #bool_series = pd.isnull(cabinet_csv_file["house"])
 
